75e2f9b6b40a9427fa45e3d96f40520230a79509
Devon Meunier <devon@ajah.ca>
2014-11-25 12:05:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,21 @@
-from distutils.core import setup
-#import markdown2
+from setuptools import setup
+
 import purifier
 
-#with open('README.md') as fh:
-#    long_description = fh.read()
-#markdowner = markdown2.Markdown()
 
 setup(
-    name = "html-purifier",
-    version = purifier.__version__,
-    packages = ["purifier"],
-    url = 'https://github.com/PixxxeL/python-html-purifier',
-    author = 'pixel',
-    author_email = 'ivan.n.sergeev@gmail.com',
-    maintainer = 'pixel',
-    maintainer_email = 'ivan.n.sergeev@gmail.com',
-    license = 'GPL3',
-    description = 'Cuts the tags and attributes from HTML that are not in the whitelist. Their content is left.',
-    #long_description = markdowner.convert(long_description),
-    download_url = 'https://github.com/PixxxeL/python-html-purifier/archive/master.zip',
-    classifiers = [
+    name="html-purifier3",
+    version=purifier.__version__,
+    packages=["purifier"],
+    url='https://github.com/PixxxeL/python-html-purifier',
+    author='pixel',
+    author_email='ivan.n.sergeev@gmail.com',
+    maintainer='meunierd',
+    maintainer_email='devon@eventmobi.com',
+    license='GPL3',
+    description='''Cuts the tags and attributes from HTML that are not in the
+    whitelist. Their content is left.''',
+    classifiers=[
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
